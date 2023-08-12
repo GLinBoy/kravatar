89d4a276488a7674805be179f00493664ea6a018
GLinBoy <hojjatabedie@gmail.com>
2023-08-12 12:23:08 +0200

    🕯 START: Initiated the project
    resolved #1

---
+++ src/main/java/com/glinboy/kavatar/KavatarApplication.java
@@ -0,0 +1,13 @@
+package com.glinboy.kavatar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KavatarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KavatarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glinboy/kavatar/KavatarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glinboy.kavatar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KavatarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
