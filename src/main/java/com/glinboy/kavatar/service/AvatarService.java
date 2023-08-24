02bb38ad53ffa77769d16e3a7628058b4db0b690
GLinBoy <hojjatabedie@gmail.com>
2023-08-24 18:14:49 +0200

    👌 IMPROVE: Added AvatarService interface methods

--- src/main/java/com/glinboy/kavatar/service/AvatarService.java
+++ src/main/java/com/glinboy/kavatar/service/AvatarService.java
@@ -1,4 +1,13 @@
 package com.glinboy.kavatar.service;
 
+import com.glinboy.kavatar.service.dto.AvatarDTO;
+import org.springframework.http.codec.multipart.FilePart;
+import reactor.core.publisher.Mono;
+
 public interface AvatarService {
+	Mono<AvatarDTO> getAvatar();
+	Mono<AvatarDTO> getAvatar(String id);
+	Mono<String> saveAvatar(Mono<FilePart> filePartMono);
+	Mono<Void> deleteAvatar(String id);
+	Mono<AvatarDTO> getDefaultAvatar();
 }
