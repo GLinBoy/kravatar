be674d2481fb3ebc7a09515a89287acf1fe35477
GLinBoy <hojjatabedie@gmail.com>
2023-08-25 17:04:30 +0200

    👌 IMPROVE: Get avatar return Optional

--- src/main/java/com/glinboy/kavatar/service/AvatarService.java
+++ src/main/java/com/glinboy/kavatar/service/AvatarService.java
@@ -4,9 +4,11 @@ import com.glinboy.kavatar.service.dto.AvatarDTO;
 import org.springframework.http.codec.multipart.FilePart;
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 public interface AvatarService {
-	Mono<AvatarDTO> getAvatar();
-	Mono<AvatarDTO> getAvatar(String id);
+	Mono<Optional<AvatarDTO>> getAvatar();
+	Mono<Optional<AvatarDTO>> getAvatar(String id);
 	Mono<String> saveAvatar(Mono<FilePart> filePartMono);
 
 	Mono<Void> deleteAvatar();
