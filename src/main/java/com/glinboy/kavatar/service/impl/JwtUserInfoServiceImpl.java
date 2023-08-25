3adfd10864cd9301f6b0e3773cd9cc21fdb8bc09
GLinBoy <hojjatabedie@gmail.com>
2023-08-25 17:09:10 +0200

    👌 IMPROVE: Added implementation of the UserServiceInfo by dummy data

--- src/main/java/com/glinboy/kavatar/service/impl/JwtUserInfoServiceImpl.java
+++ src/main/java/com/glinboy/kavatar/service/impl/JwtUserInfoServiceImpl.java
@@ -1,7 +1,20 @@
 package com.glinboy.kavatar.service.impl;
 
+import com.glinboy.kavatar.service.UserInfoService;
+import com.glinboy.kavatar.service.dto.UserInfoDTO;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JwtUserInfoServiceImpl {
+public class JwtUserInfoServiceImpl implements UserInfoService {
+	@Override
+	public UserInfoDTO getUserInfo() {
+		return new UserInfoDTO(
+			"00000000000000000000000000000000",
+			"John",
+			"Doe",
+			"John Doe",
+			"john",
+			"john.doe@email.com"
+		);
+	}
 }
