8769efd92f0fbd1eb5bd911994e37a08a5312c6c
GLinBoy <hojjatabedie@gmail.com>
2023-08-25 17:15:46 +0200

    📦 NEW: Added avatar resource

---
+++ src/main/java/com/glinboy/kavatar/web/rest/AvatarResource.java
@@ -0,0 +1,60 @@
+package com.glinboy.kavatar.web.rest;
+
+import com.glinboy.kavatar.service.AvatarService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/avatars")
+public class AvatarResource {
+
+	private final AvatarService service;
+
+	@GetMapping
+	public Mono<ResponseEntity<Flux<DataBuffer>>> getAvatar() {
+		return service.getAvatar()
+			.map(avatarDTO ->
+				avatarDTO.map(a -> ResponseEntity.ok().contentType(MediaType.valueOf(a.fileType())).body(a.fileContent()))
+					.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND))
+			);
+	}
+
+	@GetMapping("/{id}")
+	public Mono<ResponseEntity<Flux<DataBuffer>>> getAvatar(@PathVariable String id) {
+		return service.getAvatar(id)
+			.map(avatarDTO ->
+				avatarDTO.map(a -> ResponseEntity.ok().contentType(MediaType.valueOf(a.fileType())).body(a.fileContent()))
+					.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND))
+			);
+	}
+
+	@PostMapping
+	public Mono<ResponseEntity<Void>> uploadImage(@RequestPart("file") Mono<FilePart> file, ServerWebExchange exchange) {
+		log.info("URI: {}", exchange.getRequest().getURI());
+		return service.saveAvatar(file).flatMap(filename -> {
+			String imageUrl = exchange.getRequest().getURI() + "/" + filename;
+			return Mono.just(ResponseEntity.created(URI.create(imageUrl)).build());
+		});
+	}
+
+	@DeleteMapping
+	public Mono<ResponseEntity<Void>> deleteAvatar() {
+		return service
+			.deleteAvatar()
+			.then(Mono.just(ResponseEntity.noContent().build()));
+	}
+}
