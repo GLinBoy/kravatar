f105eadf03618c8a2ebce222a30903a21d202786
GLinBoy <hojjatabedie@gmail.com>
2023-08-25 17:03:12 +0200

    👌 IMPROVE: Updated file content of the avatar DTO

--- src/main/java/com/glinboy/kavatar/service/dto/AvatarDTO.java
+++ src/main/java/com/glinboy/kavatar/service/dto/AvatarDTO.java
@@ -1,10 +1,11 @@
 package com.glinboy.kavatar.service.dto;
 
-import java.awt.image.DataBuffer;
+import org.springframework.core.io.buffer.DataBuffer;
+import reactor.core.publisher.Flux;
 
 public record AvatarDTO(
 	String userId,
 	String fileType,
-	DataBuffer fileContent
+	Flux<DataBuffer> fileContent
 ) {
 }
