e43a989ed04df48f87123689f523bdde0703a409
GLinBoy <hojjatabedie@gmail.com>
2023-08-26 15:43:20 +0200

    👌 IMPROVE: Handled save method into database

--- src/main/java/com/glinboy/kavatar/service/impl/MongoAvatarServiceImpl.java
+++ src/main/java/com/glinboy/kavatar/service/impl/MongoAvatarServiceImpl.java
@@ -1,7 +1,12 @@
 package com.glinboy.kavatar.service.impl;
 
+import com.glinboy.kavatar.entity.Avatar;
+import com.glinboy.kavatar.repository.AvatarRepository;
 import com.glinboy.kavatar.service.AvatarService;
+import com.glinboy.kavatar.service.UserInfoService;
 import com.glinboy.kavatar.service.dto.AvatarDTO;
+import lombok.RequiredArgsConstructor;
+import org.bson.types.Binary;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Service;
@@ -10,8 +15,13 @@ import reactor.core.publisher.Mono;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 @ConditionalOnProperty(value = "application.avatars.storage", havingValue = "mongo")
 public class MongoAvatarServiceImpl implements AvatarService {
+
+	private final AvatarRepository repository;
+	private final UserInfoService userInfoService;
+
 	@Override
 	public Mono<Optional<AvatarDTO>> getAvatar() {
 		return null;
@@ -24,7 +34,24 @@ public class MongoAvatarServiceImpl implements AvatarService {
 
 	@Override
 	public Mono<String> saveAvatar(Mono<FilePart> filePartMono) {
-		return null;
+		String userId = userInfoService.getUserInfo().id();
+		return filePartMono
+			.flatMap(filePart -> {
+				filePart.content().flatMap(dataBuffer -> {
+						byte[] bytes = new byte[dataBuffer.readableByteCount()];
+						dataBuffer.read(bytes);
+						return Mono.just(bytes);
+					}).last()
+					.map(imageBytes ->
+						repository.save(
+							Avatar.builder()
+								.userId(userId)
+								.fileType(filePart.headers().getContentType().toString())
+								.fileContent(new Binary(imageBytes))
+								.build()
+						));
+				return Mono.just(userId);
+			});
 	}
 
 	@Override
