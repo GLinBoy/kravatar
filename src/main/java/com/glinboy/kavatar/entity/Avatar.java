299dba91386f3978420e9dcacaf3801985aaa3c6
GLinBoy <hojjatabedie@gmail.com>
2023-08-26 15:38:10 +0200

    📦 NEW: Added Avatar entity
    resolved #52

---
+++ src/main/java/com/glinboy/kavatar/entity/Avatar.java
@@ -0,0 +1,21 @@
+package com.glinboy.kavatar.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.Binary;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@Document
+@NoArgsConstructor
+@AllArgsConstructor
+public class Avatar {
+	@Id
+	private String userId;
+	private String fileType;
+	private Binary fileContent;
+}
