42b93198c80f88ea672e61ebe9014a5eea1e6265
GLinBoy <hojjatabedie@gmail.com>
2023-08-29 19:48:46 +0200

    👌 IMPROVE: Extracted user info data from JWT token

--- src/main/java/com/glinboy/kavatar/service/impl/JwtUserInfoServiceImpl.java
+++ src/main/java/com/glinboy/kavatar/service/impl/JwtUserInfoServiceImpl.java
@@ -2,19 +2,25 @@ package com.glinboy.kavatar.service.impl;
 
 import com.glinboy.kavatar.service.UserInfoService;
 import com.glinboy.kavatar.service.dto.UserInfoDTO;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtUserInfoServiceImpl implements UserInfoService {
 	@Override
 	public UserInfoDTO getUserInfo() {
+		JwtAuthenticationToken authenticationToken =
+			(JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+		Jwt jwt = (Jwt) authenticationToken.getCredentials();
 		return new UserInfoDTO(
-			"00000000000000000000000000000000",
-			"John",
-			"Doe",
-			"John Doe",
-			"john",
-			"john.doe@email.com"
+			(String) jwt.getClaims().get("sub"),
+			(String) jwt.getClaims().get("given_name"),
+			(String) jwt.getClaims().get("family_name"),
+			(String) jwt.getClaims().get("name"),
+			(String) jwt.getClaims().get("preferred_username"),
+			(String) jwt.getClaims().get("email")
 		);
 	}
 }
