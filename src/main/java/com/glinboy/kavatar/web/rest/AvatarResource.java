40ec8859adf13c790f2c9a45d7a7f7fb37225ca5
GLinBoy <hojjatabedie@gmail.com>
2023-09-11 19:19:19 +0200

    👌 IMPROVE: Updated return type of methods and consumed methods

--- src/main/java/com/glinboy/kavatar/web/rest/AvatarResource.java
+++ src/main/java/com/glinboy/kavatar/web/rest/AvatarResource.java
@@ -1,17 +1,14 @@
 package com.glinboy.kavatar.web.rest;
 
 import com.glinboy.kavatar.service.AvatarService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.net.URI;
 
@@ -23,35 +20,31 @@ public class AvatarResource {
 	private final AvatarService service;
 
 	@GetMapping
-	public Mono<ResponseEntity<Flux<DataBuffer>>> getAvatar() {
+	public ResponseEntity<byte[]> getAvatar() {
 		return service.getAvatar()
 			.map(avatarDTO ->
-				avatarDTO.map(a -> ResponseEntity.ok().contentType(MediaType.valueOf(a.fileType())).body(a.fileContent()))
-					.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND))
-			);
+				ResponseEntity.ok().contentType(MediaType.valueOf(avatarDTO.fileType())).body(avatarDTO.fileContent())
+			).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 
 	@GetMapping("/{id}")
-	public Mono<ResponseEntity<Flux<DataBuffer>>> getAvatar(@PathVariable String id) {
+	public ResponseEntity<byte[]> getAvatar(@PathVariable String id) {
 		return service.getAvatar(id)
 			.map(avatarDTO ->
-				avatarDTO.map(a -> ResponseEntity.ok().contentType(MediaType.valueOf(a.fileType())).body(a.fileContent()))
-					.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND))
-			);
+				ResponseEntity.ok().contentType(MediaType.valueOf(avatarDTO.fileType())).body(avatarDTO.fileContent())
+			).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 
 	@PostMapping
-	public Mono<ResponseEntity<Void>> uploadImage(@RequestPart("file") Mono<FilePart> file, ServerWebExchange exchange) {
-		return service.saveAvatar(file).flatMap(filename -> {
-			String imageUrl = exchange.getRequest().getURI() + "/" + filename;
-			return Mono.just(ResponseEntity.created(URI.create(imageUrl)).build());
-		});
+	public ResponseEntity<Void> uploadImage(@RequestPart("file") MultipartFile file, HttpServletRequest request) {
+		String idName = service.saveAvatar(file);
+		String imageUrl = request.getRequestURL().toString() + "/" + idName;
+		return ResponseEntity.created(URI.create(imageUrl)).build();
 	}
 
 	@DeleteMapping
-	public Mono<ResponseEntity<Void>> deleteAvatar() {
-		return service
-			.deleteAvatar()
-			.then(Mono.just(ResponseEntity.noContent().build()));
+	public ResponseEntity<Void> deleteAvatar() {
+		service.deleteAvatar();
+		return ResponseEntity.noContent().build();
 	}
 }
