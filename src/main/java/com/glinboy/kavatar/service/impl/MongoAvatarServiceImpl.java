7f39e93deae21abe80ed3e4ae26c39eac3ae49c7
GLinBoy <hojjatabedie@gmail.com>
2023-09-12 17:10:09 +0200

    👌 IMPROVE: Replaced reactive implementations in MongoAvatarServiceImpl

--- src/main/java/com/glinboy/kavatar/service/impl/MongoAvatarServiceImpl.java
+++ src/main/java/com/glinboy/kavatar/service/impl/MongoAvatarServiceImpl.java
@@ -1,20 +1,15 @@
 package com.glinboy.kavatar.service.impl;
 
-import com.glinboy.kavatar.entity.Avatar;
 import com.glinboy.kavatar.repository.AvatarRepository;
 import com.glinboy.kavatar.service.AvatarService;
 import com.glinboy.kavatar.service.UserInfoService;
 import com.glinboy.kavatar.service.dto.AvatarDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.bson.types.Binary;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.core.io.buffer.DataBufferUtils;
-import org.springframework.core.io.buffer.DefaultDataBufferFactory;
-import org.springframework.http.MediaType;
-import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
@@ -31,57 +26,41 @@ public class MongoAvatarServiceImpl implements AvatarService {
 	private final UserInfoService userInfoService;
 
 	@Override
-	public Mono<Optional<AvatarDTO>> getAvatar() {
+	public Optional<AvatarDTO> getAvatar() {
 		return null;
 	}
 
 	@Override
-	public Mono<Optional<AvatarDTO>> getAvatar(String id) {
+	public Optional<AvatarDTO> getAvatar(String id) {
 		return null;
 	}
 
 	@Override
-	public Mono<String> saveAvatar(Mono<FilePart> filePartMono) {
+	public String saveAvatar(MultipartFile multipartFile) {
 		String userId = userInfoService.getUserInfo().id();
-		return filePartMono
-			.flatMap(filePart -> {
-				filePart.content().flatMap(dataBuffer -> {
-						byte[] bytes = new byte[dataBuffer.readableByteCount()];
-						dataBuffer.read(bytes);
-						return Mono.just(bytes);
-					})
-					.last()
-					.map(imageBytes ->
-						repository.save(
-							Avatar.builder()
-								.userId(userId)
-								.fileType(
-									Optional.ofNullable(filePart.headers().getContentType())
-										.orElseGet(() -> MediaType.IMAGE_JPEG)
-										.toString()
-								)
-								.fileContent(new Binary(imageBytes))
-								.build()
-						));
-				return Mono.just(userId);
-			});
+
+		return userId;
 	}
 
 	@Override
-	public Mono<Void> deleteAvatar() {
-		return repository.deleteById(userInfoService.getUserInfo().id());
+	public void deleteAvatar() {
+		repository.deleteById(userInfoService.getUserInfo().id());
 	}
 
 	@Override
-	public Mono<AvatarDTO> getDefaultAvatar() {
+	public Optional<AvatarDTO> getDefaultAvatar() {
 		try {
-			File file = new File(this.getClass().getResource("/static/images/default_avatar.jpg").getFile());
-			return Mono.just(new AvatarDTO(userInfoService.getUserInfo().id(), Files.probeContentType(file.toPath()),
-				DataBufferUtils.read(file.toPath(), new DefaultDataBufferFactory(), 64 * 1024))
+			File file = ResourceUtils.getFile("/static/images/default_avatar.jpg");
+			return Optional.of(
+				new AvatarDTO(
+					userInfoService.getUserInfo().id(),
+					Files.probeContentType(file.toPath()),
+					Files.readAllBytes(file.toPath())
+				)
 			);
 		} catch (IOException | NullPointerException ex) {
 			log.error("Can not load default user avatar file", ex);
 		}
-		return Mono.empty();
+		return Optional.empty();
 	}
 }
