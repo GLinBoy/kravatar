ea5dc7c624ae9fd29b583e170fb165811f11fd25
GLinBoy <hojjatabedie@gmail.com>
2023-09-12 17:54:11 +0200

    📦 NEW: Added Profile service impl

---
+++ src/main/java/com/glinboy/kavatar/service/impl/ProfileServiceImpl.java
@@ -0,0 +1,8 @@
+package com.glinboy.kavatar.service.impl;
+
+import com.glinboy.kavatar.service.ProfileService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfileServiceImpl implements ProfileService {
+}
