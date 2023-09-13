34ff820ba3c5354e5aba9eb9b87a8f9d6e7b123d
GLinBoy <hojjatabedie@gmail.com>
2023-09-13 19:53:21 +0200

    👌 IMPROVE: Added get current user profile method to ProfileResource

--- src/main/java/com/glinboy/kavatar/web/rest/ProfileResource.java
+++ src/main/java/com/glinboy/kavatar/web/rest/ProfileResource.java
@@ -1,9 +1,23 @@
 package com.glinboy.kavatar.web.rest;
 
+import com.glinboy.kavatar.service.ProfileService;
+import com.glinboy.kavatar.service.dto.UserInfoDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/profiles")
+@RequestMapping("/api/profiles")
+@RequiredArgsConstructor
 public class ProfileResource {
+	private final ProfileService service;
+
+	@GetMapping
+	public ResponseEntity<UserInfoDTO> getProfile() {
+		return service.getProfile()
+			.map(ResponseEntity::ok)
+			.orElseGet(() -> ResponseEntity.notFound().build());
+	}
 }
