9380009aee04f99d03b7a23b5a352ff5f1a80083
GLinBoy <hojjatabedie@gmail.com>
2023-09-13 19:49:29 +0200

    👌 IMPROVE: Updated UserInfoDTO to be usable in Profile service

--- src/main/java/com/glinboy/kavatar/service/dto/UserInfoDTO.java
+++ src/main/java/com/glinboy/kavatar/service/dto/UserInfoDTO.java
@@ -1,11 +1,19 @@
 package com.glinboy.kavatar.service.dto;
 
+import java.util.List;
+import java.util.Map;
+
 public record UserInfoDTO(
 	String id,
 	String name,
 	String family,
-	String fullName,
 	String username,
-	String email
+	String email,
+	Boolean emailVerified,
+	Long createdAt,
+	Map<String, List<String>> attributes,
+	List<String> groups,
+	List<String> realmRoles,
+	Map<String, List<String>> clientRoles
 	) {
 }
