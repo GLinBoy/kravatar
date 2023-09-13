920f814a7a3bf8913c7b7d354a63ad2664a5ce2c
GLinBoy <hojjatabedie@gmail.com>
2023-09-13 19:52:04 +0200

    👌 IMPROVE: Added a method to get current user profile

--- src/main/java/com/glinboy/kavatar/service/ProfileService.java
+++ src/main/java/com/glinboy/kavatar/service/ProfileService.java
@@ -1,4 +1,9 @@
 package com.glinboy.kavatar.service;
 
+import com.glinboy.kavatar.service.dto.UserInfoDTO;
+
+import java.util.Optional;
+
 public interface ProfileService {
+    Optional<UserInfoDTO> getProfile();
 }
