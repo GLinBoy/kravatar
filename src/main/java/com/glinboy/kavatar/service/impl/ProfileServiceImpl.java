e8754251b48e2a96c67ef9fdb3e1c274530c4b51
GLinBoy <hojjatabedie@gmail.com>
2023-09-15 17:15:51 +0200

    👌 IMPROVE: Added get userinfo by ID method to ProfileServiceImpl

--- src/main/java/com/glinboy/kavatar/service/impl/ProfileServiceImpl.java
+++ src/main/java/com/glinboy/kavatar/service/impl/ProfileServiceImpl.java
@@ -3,12 +3,14 @@ package com.glinboy.kavatar.service.impl;
 import com.glinboy.kavatar.service.ProfileService;
 import com.glinboy.kavatar.service.UserInfoService;
 import com.glinboy.kavatar.service.dto.UserInfoDTO;
+import jakarta.ws.rs.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -41,5 +43,29 @@ public class ProfileServiceImpl implements ProfileService {
 			representation.getRealmRoles(),
 			representation.getClientRoles()
 		));
+	@Override
+	public Optional<UserInfoDTO> getProfile(String id) {
+		try {
+			UserRepresentation representation = keycloak.realm(realm)
+				.users()
+				.get(id)
+				.toRepresentation();
+			return Optional.of(new UserInfoDTO(
+				representation.getId(),
+				representation.getFirstName(),
+				representation.getLastName(),
+				representation.getUsername(),
+				representation.getEmail(),
+				representation.isEmailVerified(),
+				representation.getCreatedTimestamp(),
+				representation.getAttributes(),
+				representation.getGroups(),
+				representation.getRealmRoles(),
+				representation.getClientRoles()
+			));
+		} catch (NotFoundException ex) {
+			throw new ResponseStatusException(ex.getResponse().getStatus(),
+				"Can not found user id: %s".formatted(id), ex);
+		}
 	}
 }
