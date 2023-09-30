c79823dab7b54ce741d556d79b7e2f465950ce36
GLinBoy <hojjatabedie@gmail.com>
2023-09-30 15:14:00 +0200

    👌 IMPROVE: Added a method to generate QR from URL

--- src/main/java/com/glinboy/kavatar/util/GeneratorUtils.java
+++ src/main/java/com/glinboy/kavatar/util/GeneratorUtils.java
@@ -1,12 +1,21 @@
 package com.glinboy.kavatar.util;
 
 import com.glinboy.kavatar.service.dto.UserInfoDTO;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import ezvcard.Ezvcard;
 import ezvcard.VCard;
 import ezvcard.property.StructuredName;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
@@ -31,5 +40,17 @@ public final class GeneratorUtils {
 				));
 			})
 			.orElseGet(Optional::empty);
+	}
+
+	public static Optional<byte[]> qrGenerate(String url) {
+		try {
+			QRCodeWriter qrCodeWriter = new QRCodeWriter();
+			BitMatrix bitMatrix = qrCodeWriter.encode(url, BarcodeFormat.QR_CODE, 250, 250);
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+			return Optional.of(outputStream.toByteArray());
+		} catch (IOException | WriterException ex) {
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Can't generate QR", ex);
+		}
 	}
 }
