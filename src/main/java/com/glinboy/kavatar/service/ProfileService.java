d2e6ee9797bb4164f205839d8bdd68a072cd1055
GLinBoy <hojjatabedie@gmail.com>
2023-09-30 15:39:47 +0200

    👌 IMPROVE: Removed getProfile method without input

--- src/main/java/com/glinboy/kavatar/service/ProfileService.java
+++ src/main/java/com/glinboy/kavatar/service/ProfileService.java
@@ -5,7 +5,5 @@ import com.glinboy.kavatar.service.dto.UserInfoDTO;
 import java.util.Optional;
 
 public interface ProfileService {
-    Optional<UserInfoDTO> getProfile();
-
 	Optional<UserInfoDTO> getProfile(String id);
 }
