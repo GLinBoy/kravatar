f5348cc3ef3b530a130c809b3371a1f47972442e
GLinBoy <hojjatabedie@gmail.com>
2023-09-30 15:33:30 +0200

    👌 IMPROVE: Used GeneratorUtil to generate QR

--- src/main/java/com/glinboy/kavatar/web/rest/ProfileResource.java
+++ src/main/java/com/glinboy/kavatar/web/rest/ProfileResource.java
@@ -3,25 +3,16 @@ package com.glinboy.kavatar.web.rest;
 import com.glinboy.kavatar.service.ProfileService;
 import com.glinboy.kavatar.service.dto.UserInfoDTO;
 import com.glinboy.kavatar.util.GeneratorUtils;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.Optional;
 
 @RestController
@@ -64,17 +55,14 @@ public class ProfileResource {
 	@GetMapping(value = "/{id}.qr", produces = MediaType.IMAGE_PNG_VALUE)
 	public ResponseEntity<byte[]> getProfileQr(@PathVariable String id, HttpServletRequest request) {
 		// FIXME Generate QR to redirect to user profile page
-		try {
-			QRCodeWriter qrCodeWriter = new QRCodeWriter();
-			BitMatrix bitMatrix = qrCodeWriter.encode(request.getRequestURL().toString().replace(".qr", ""), BarcodeFormat.QR_CODE, 250, 250);
-			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-			MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
-			return ResponseEntity
-				.ok()
-				.contentType(MediaType.IMAGE_PNG)
-				.body(outputStream.toByteArray());
-		} catch (IOException | WriterException ex) {
-			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Can't generate QR", ex);
-		}
+		return GeneratorUtils
+			.qrGenerate(request.getRequestURL().toString().replace(".qr", ""))
+			.map(v ->
+				ResponseEntity
+					.ok()
+					.contentType(MediaType.IMAGE_PNG)
+					.body(v)
+			)
+			.orElseGet(() -> ResponseEntity.notFound().build());
 	}
 }
