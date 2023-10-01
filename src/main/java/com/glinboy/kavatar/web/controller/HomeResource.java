a0753b85abdb2a642d93372c272abc0d82d54d1c
GLinBoy <hojjatabedie@gmail.com>
2023-10-01 17:46:26 +0200

    👌 IMPROVE: Added user profile page to the HomeResource

--- src/main/java/com/glinboy/kavatar/web/controller/HomeResource.java
+++ src/main/java/com/glinboy/kavatar/web/controller/HomeResource.java
@@ -2,6 +2,7 @@ package com.glinboy.kavatar.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +12,12 @@ public class HomeResource {
 	@GetMapping
 	public String getHomePage() {
 		return "index";
+	}
+
+
+	@GetMapping("/{id}")
+	public String getProfilePage(@PathVariable String id) {
+		return "profile";
 	}
 
 }
